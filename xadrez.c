e0eb2538cc727a2b576dc17b126df1adaef977ae
rogeaportu <80826419+rogeaportu@users.noreply.github.com>
2025-06-02 03:33:26 +0000

    Modificando o projeto adcionando funções recursivas

--- xadrez.c
+++ xadrez.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+// adição das funções recursivas
+
+
 // Desafio de Xadrez - MateCheck
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
